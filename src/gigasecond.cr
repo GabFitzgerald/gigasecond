8a22a0225a59e213e7369ac8e73c392221b57806
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-10 16:47:40 +1000

    solved

--- src/gigasecond.cr
+++ src/gigasecond.cr
@@ -1,19 +1,6 @@
 # Please implement your solution to gigasecond in this file
 class Gigasecond 
     def self.from(moment : Time) 
-        # convert moment to seconds
-        return moment.to(Int32)
-        # add a gigasecond
-
-        # convert seconds to Time
-
-        # gigasecond = Time.utc(2001, 9, 9, 11, 46, 40)
-        # return moment + gigasecond
+        return moment + (10**9).seconds
     end
-
-    puts from(Time.utc(2011, 4, 25, 0, 0, 0))
-    puts Time.utc(2011, 4, 25, 0, 0, 0)
-    puts Time.now
 end
-
-# 2001-09-09 11:46:40 +1000
